4b84ad701aea3c3711bb4f7820f8abd3c88df352
artem2090-vl <v.artem2090@gmail.com>
2025-03-07 20:13:01 +0200

    add news file

--- firstsite/main/views.py
+++ firstsite/main/views.py
@@ -4,7 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title' : 'Головна сторінка',
+        'values' : ['Hello', 'World', 'Django', 'Python', 'HTML', 'CSS'],
+        'obj' : {
+            'model' : 'BMW',
+            'color' : 'black',
+            'doors' : 4,}
+    }
+    return render(request, 'main/index.html', data)
 
 
 def about(request):
