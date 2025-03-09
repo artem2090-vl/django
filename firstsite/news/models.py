c01a80dcda78a4178afda51d20936881cedac948
artem2090-vl <v.artem2090@gmail.com>
2025-03-09 23:48:53 +0200

    add base news

--- firstsite/news/models.py
+++ firstsite/news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField('Назва', max_length=50)
+    anons = models.CharField('Анонс', max_length=250)
+    full_text = models.TextField('Текст')
+    date = models.DateTimeField('Дата публікації')
+    def __str__(self):
+        return self.title
+
